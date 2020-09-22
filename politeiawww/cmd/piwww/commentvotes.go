8b41e3176dc64cbbcccff5a66681e9b727efdfbe
amass <amassarwi@gmail.com>
2020-09-22 18:25:19 +0300

    ditch state flags

--- politeiawww/cmd/piwww/commentvotes.go
+++ politeiawww/cmd/piwww/commentvotes.go
@@ -5,8 +5,6 @@
 package main
 
 import (
-	"fmt"
-
 	pi "github.com/decred/politeia/politeiawww/api/pi/v1"
 	"github.com/decred/politeia/politeiawww/cmd/shared"
 )
@@ -18,10 +16,6 @@ type CommentVotesCmd struct {
 		Token  string `positional-arg-name:"token"`  // Censorship token
 		UserID string `positional-arg-name:"userid"` // User id
 	} `positional-args:"true" required:"true"`
-
-	// CLI flags
-	Vetted   bool `long:"vetted" optional:"true"`
-	Unvetted bool `long:"unvetted" optional:"true"`
 }
 
 // Execute executes the user comment likes command.
@@ -29,22 +23,9 @@ func (cmd *CommentVotesCmd) Execute(args []string) error {
 	token := cmd.Args.Token
 	userID := cmd.Args.UserID
 
-	// Verify state
-	var state pi.PropStateT
-	switch {
-	case cmd.Vetted && cmd.Unvetted:
-		return fmt.Errorf("cannot use --vetted and --unvetted simultaneously")
-	case cmd.Unvetted:
-		state = pi.PropStateUnvetted
-	case cmd.Vetted:
-		state = pi.PropStateVetted
-	default:
-		return fmt.Errorf("must specify either --vetted or unvetted")
-	}
-
 	cvr, err := client.CommentVotes(pi.CommentVotes{
 		Token:  token,
-		State:  state,
+		State:  pi.PropStateVetted,
 		UserID: userID,
 	})
 	if err != nil {
@@ -62,8 +43,4 @@ Get the provided user comment upvote/downvotes for a proposal.
 Arguments:
 1. token       (string, required)  Proposal censorship token
 2. userid      (string, required)  User id
-
-Flags:
-  --vetted     (bool, optional)    Comment on vetted record.
-  --unvetted   (bool, optional)    Comment on unvetted reocrd.
 `
