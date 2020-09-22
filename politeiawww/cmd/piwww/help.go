0b8f04d887044008542e2f8d022d01f7fd71bb0a
amass <amassarwi@gmail.com>
2020-09-22 17:11:18 +0300

    politeiawww: add processCommentVotes

--- politeiawww/cmd/piwww/help.go
+++ politeiawww/cmd/piwww/help.go
@@ -95,8 +95,8 @@ func (cmd *HelpCmd) Execute(args []string) error {
 		fmt.Printf("%s\n", commentCensorHelpMsg)
 	case "commentvote":
 		fmt.Printf("%s\n", commentVoteHelpMsg)
-	case "userlikecomments":
-		fmt.Printf("%s\n", userLikeCommentsHelpMsg)
+	case "usercommentvotes":
+		fmt.Printf("%s\n", userCommentVotesHelpMsg)
 
 	// Vote commands
 	case "authorizevote":
--- politeiawww/cmd/piwww/userlikecomments.go
+++ politeiawww/cmd/piwww/userlikecomments.go
@@ -4,42 +4,62 @@
 
 package main
 
-import "github.com/decred/politeia/politeiawww/cmd/shared"
+import (
+	"fmt"
 
-// UserLikeCommentsCmd retreives the logged in user's like comment objects for
+	pi "github.com/decred/politeia/politeiawww/api/pi/v1"
+	"github.com/decred/politeia/politeiawww/cmd/shared"
+)
+
+// UserCommentVotesCmd retreives the logged in user's like comment objects for
 // the specified proposal.
-type UserLikeCommentsCmd struct {
+type UserCommentVotesCmd struct {
 	Args struct {
 		Token string `positional-arg-name:"token"` // Censorship token
 	} `positional-args:"true" required:"true"`
+
+	// CLI flags
+	Vetted   bool `long:"vetted" optional:"true"`
+	Unvetted bool `long:"unvetted" optional:"true"`
 }
 
 // Execute executes the user comment likes command.
-func (cmd *UserLikeCommentsCmd) Execute(args []string) error {
-	cvr, err := client.UserCommentsLikes(cmd.Args.Token)
+func (cmd *UserCommentVotesCmd) Execute(args []string) error {
+	token := cmd.Args.Token
+
+	// Verify state
+	var state pi.PropStateT
+	switch {
+	case cmd.Vetted && cmd.Unvetted:
+		return fmt.Errorf("cannot use --vetted and --unvetted simultaneously")
+	case cmd.Unvetted:
+		state = pi.PropStateUnvetted
+	case cmd.Vetted:
+		state = pi.PropStateVetted
+	default:
+		return fmt.Errorf("must specify either --vetted or unvetted")
+	}
+
+	cvr, err := client.UserCommentVotes(pi.CommentVotes{
+		Token: token,
+		State: state,
+	})
 	if err != nil {
 		return err
 	}
 	return shared.PrintJSON(cvr)
 }
 
-// userLikeCommentsHelpMsg is the output for the help command when
+// userCommentVotesHelpMsg is the output for the help command when
 // 'userlikecomments' is specified.
-const userLikeCommentsHelpMsg = `userlikecomments "token"
+const userCommentVotesHelpMsg = `usercommentvotes "token"
 
 Get the logged in user's comment upvote/downvotes for a proposal.
 
 Arguments:
 1. token       (string, required)  Proposal censorship token
 
-Result:
-
-{
-  "commentslikes": [
-    {
-      "action":    (string)  Vote (upvote or downvote)
-      "commentid"  (string)  ID of the comment
-      "token":     (string)  Proposal censorship token
-    },
-  ]
-}`
+Flags:
+  --vetted     (bool, optional)    Comment on vetted record.
+  --unvetted   (bool, optional)    Comment on unvetted reocrd.
+`
