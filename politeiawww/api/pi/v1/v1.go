8c9c237e5e01d06eed49df7094d0086b95174e51
luke <lukebp@users.noreply.github.com>
2021-07-19 16:15:40 -0500

    Design proposal statuses.

--- politeiawww/api/pi/v1/v1.go
+++ politeiawww/api/pi/v1/v1.go
@@ -63,3 +63,67 @@ type VoteMetadata struct {
 	// in the runoff vote.
 	LinkTo string `json:"linkto,omitempty"`
 }
+
+// PropStatusT is used to indicate what part of the proposal life cycle a
+// proposal is currently in. It combines record statuses, ticketvote statuses,
+// as well as adding in statuses that are specific to the Decred proposal
+// system in order to create a unified map of the various paths a proposal can
+// take throughout the proposal process. This serves as the source of truth for
+// clients so that they don't need to try and decipher what various
+// combinations of plugin statuses mean for the proposal.
+type PropStatusT uint32
+
+const (
+	// PropStatusInvalid represents and invalid proposal status.
+	PropStatusInvalid PropStatusT = 0
+
+	// PropStatusUnvetted represents a proposal that has been submitted
+	// but has not yet been made public by the admins.
+	PropStatusUnvetted PropStatusT = 1
+
+	// PropStatusUnvettedAbandoned represents a proposal that has been
+	// submitted, but was abandoned by the author prior to being made
+	// public by the admins. The proposal can be marked as abandoned by
+	// either the proposal author or an admin. Abandoned proposal files
+	// are not deleted from the backend, but on additional proposal or
+	// plugin data changes can be made.
+	PropStatusUnvettedAbandoned PropStatusT = 2
+
+	// PropStatusUnvettedCensored represents a proposal that has been
+	// submitted, but was censored by an admin prior to being made
+	// public. Censored proposal files are deleted from the backend.
+	// Not additional proposal or plugin data changes can be made.
+	PropStatusUnvettedCensored PropStatusT = 3
+
+	// PropStatusUnderReview represents a proposal that has been made
+	// public and is being reviewed by the Decred stakeholders, but has
+	// not had its voting period started yet.
+	PropStatusUnderReview PropStatusT = 4
+
+	// PropStatusAbandoned represents a proposal that has been made
+	// public, but has been abandoned by the proposal author.
+	// Abandoned proposals are locked from any additional changes.
+	PropStatusAbandoned PropStatusT = 5
+
+	PropStatusCensored PropStatusT = 6
+
+	PropStatusVoteStarted PropStatusT = 7
+
+	PropStatusNotFunded PropStatusT = 8
+
+	// PropStatusActive represents a proposal that was funded and is
+	// actively being billed against.
+	PropStatusActive PropStatusT = 9
+
+	// PropStatusCompleted represents a proposal that was funded and
+	// has been completed. A completed proposal is no longer being
+	// billed against. A proposal is marked as completed by an admin.
+	PropStatusCompleted PropStatusT = 10
+
+	// PropStatusClosed represents a proposal that was funded, but
+	// was never completed. A proposal is marked as closed by an admin.
+	// A closed proposal cannot be billed against any further. The most
+	// common reason a proposal would be closed is because the author
+	// failed to deliver on the milestones laid out in the proposal.
+	PropStatusClosed PropStatusT = 11
+)
